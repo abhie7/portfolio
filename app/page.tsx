e4a246766e2e1123ba9d6c61bea813eb35edcef1
Abhiraj Chaudhuri <abhirajchaudhuri@gmail.com>
2024-10-09 00:15:05 +0530

    update tsconfig

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@
 
 import { navItems } from '@/data';
 
-import Hero from '../components/Hero';
-import Grid from '../components/Grid';
-import Footer from '../components/Footer';
-// import Clients from '../components/Clients';
-import Approach from '../components/Approach';
-import Experience from '../components/Experience';
-import Projects from '../components/Projects';
-import { FloatingNav } from '../components/ui/FloatingNav';
+import Hero from '@/components/Hero';
+import Grid from '@/components/Grid';
+import Footer from '@/components/Footer';
+// import Clients from '@/components/Clients';
+import Approach from '@/components/Approach';
+import Experience from '@/components/Experience';
+import Projects from '@/components/Projects';
+import { FloatingNav } from '@/components/ui/FloatingNav';
 
 const Home = () => {
     return (
